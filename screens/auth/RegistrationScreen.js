59c56d97ac38bf037822182950107b70e6b3f632
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-22 19:53:22 +0300

    refactoring

--- screens/auth/RegistrationScreen.js
+++ screens/auth/RegistrationScreen.js
@@ -18,7 +18,6 @@ const initialState = {
 };
 
 export default function RegistrationScreen() {
-    // console.log(Platform.OS);
     const [isShowKeyboard, setIsShowKeyboard] = useState(false);
     const [state, setState] = useState(initialState);
 
@@ -28,7 +27,6 @@ export default function RegistrationScreen() {
         console.log(state);
         setState(initialState);
     };
-
     return (
         <TouchableWithoutFeedback onPress={keyboardHide}>
             <View style={styles.container}>
@@ -36,13 +34,9 @@ export default function RegistrationScreen() {
                     style={styles.image}
                     source={require("../../assets/images/BG_Mountings.jpg")}
                 >
-                        <Text style={styles.add}>+</Text>
-                        <Text style={styles.cicle}></Text>
-                    <View style={styles.avatar}>
-                    </View>
-                    <View >
-
-                    </View>
+                    <Text style={styles.add}>+</Text>
+                    <Text style={styles.cicle}></Text>
+                    <View style={styles.avatar}></View>
                     <View
                         style={{
                             ...styles.form,
@@ -52,8 +46,7 @@ export default function RegistrationScreen() {
                         <View style={styles.header}>
                             <Text style={styles.headerTitle}>Регистрация</Text>
                         </View>
-
-                        <View >
+                        <View>
                             <TextInput
                                 style={styles.input}
                                 placeholder="Логин"
@@ -105,10 +98,10 @@ export default function RegistrationScreen() {
                                 Зарегистрироваться
                             </Text>
                         </TouchableOpacity>
-                        <View  style={styles.header}>
-                        <Text style={styles.headerEnter}>
-                            Уже есть аккаунт? Войти
-                        </Text>
+                        <View style={styles.header}>
+                            <Text style={styles.headerEnter}>
+                                Уже есть аккаунт? Войти
+                            </Text>
                         </View>
                     </View>
                 </ImageBackground>
@@ -127,34 +120,34 @@ const styles = StyleSheet.create({
         resizeMode: "cover",
         justifyContent: "flex-end",
     },
-    avatar:{
+    avatar: {
         width: 120,
         height: 120,
         left: 134,
         top: 203,
-        zIndex:1,
+        zIndex: 1,
         backgroundColor: "#F6F6F6",
         borderRadius: 16,
     },
-    add:{
+    add: {
         color: "#ff6c00",
         width: 25,
         height: 25,
         left: 241,
         top: 313,
-        zIndex:2,
+        zIndex: 2,
         fontSize: 26,
-        textAlign: 'center',
+        textAlign: "center",
         fontWeight: "200",
-        transform: [{ translateY: 18 }]
+        transform: [{ translateY: 18 }],
     },
-    cicle:{
+    cicle: {
         color: "#ff6c00",
         width: 25,
         height: 25,
         left: 241,
         top: 313,
-        zIndex:2,
+        zIndex: 2,
         borderWidth: 1,
         borderColor: "#ff6c00",
         fontSize: 26,
@@ -167,10 +160,9 @@ const styles = StyleSheet.create({
         borderRadius: 6,
         fontColor: "#bdbdbd",
         marginHorizontal: 16,
-        textAlign: 'left',
-        paddingLeft:16,
-        backgroundColor: "#eaeaea"
-
+        textAlign: "left",
+        paddingLeft: 16,
+        backgroundColor: "#eaeaea",
     },
     form: {
         marginHorizontal: 0,
@@ -185,7 +177,7 @@ const styles = StyleSheet.create({
         // color: "#bdbdbd",
         marginBottom: 16,
         fontSize: 16,
-        marginLeft:0,
+        marginLeft: 0,
     },
     btn: {
         borderRadius: 100,
